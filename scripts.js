c7e60cd08bf758944882d33f8169a01ae88a8793
NeoPhetoane <phetoaneneo4@gmail.com>
2024-05-17 10:31:08 +0200

    filtering function refactored and implemented

--- scripts.js
+++ scripts.js
@@ -215,27 +215,11 @@ document
     }
 
     document.querySelector('[data-list-items]').innerHTML = ''
-    const newItems = document.createDocumentFragment()
+    renderBooks(matches.slice(
+        0, BOOKS_PER_PAGE))
 
-    for (const { author, id, image, title } of result.slice(0, BOOKS_PER_PAGE)) {
-        const element = document.createElement('button')
-        element.classList = 'preview'
-        element.setAttribute('data-preview', id)
-
-        element.innerHTML = `
-            <img
-                class="preview__image"
-                src="${image}"
-            />
-            
-            <div class="preview__info">
-                <h3 class="preview__title">${title}</h3>
-                <div class="preview__author">${authors[author]}</div>
-            </div>
-        `
-
-        newItems.appendChild(element)
-    }
+        window.scrollTo({top: 0, behavior: 'smooth'});
+        document.querySelector('[data-search-overlay]').open = false
   });
 
   //Show more button per page
